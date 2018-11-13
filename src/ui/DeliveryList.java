faf17200c3a7d1fd1eac00c2cedb5b79819b61b2
Chunyi-G <gohchunyi1997@gmail.com>
2018-11-14 02:38:44 +0800

    Initial Payment & pickup and delivery List

---
+++ src/ui/DeliveryList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+/**
+ *
+ * @author user
+ */
+public class DeliveryList {
+    
+   public void DeliveryList(){
+        System.out.println("Delivery List:");
+        System.out.println("1. Location : Sunway");
+        System.out.println("2. Location : Subang");
+        System.out.println("3. Location : Setapak");
+        System.out.println("4. Location : Klang");
+    }
+}
